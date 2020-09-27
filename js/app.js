332cc9993dfd1e1efe1591bf91d3caacdb826425
Rowan_Pereira <rp@rowanpereira.co.za>
2020-09-27 16:47:31 +0200

    Updated template files

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-// JavaScript Document
+// Fader
+
+var controller = new ScrollMagic.Controller();
+
+$(".container-fluid").each(function() {
+  var tl = new TimelineMax();
+  var child = $(this).find(".gsap-fader");
+  tl.to(child, 1, { opacity: 1, ease: Linear.easeNone });
+
+  var scene = new ScrollMagic.Scene({
+    triggerElement: this,
+    triggerHook: 0.8,
+    duration: "80%"
+  })
+    .setTween(tl)
+    .addTo(controller);
+});
